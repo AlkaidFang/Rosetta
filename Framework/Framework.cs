ef0f52ffd0cad4cb5420720d5807ebfa1b53aa29
AlkaidFang <alkaidfang@gmail.com>
2015-11-20 21:49:13 +0800

    dll project
    dll project and vs tools

---
+++ Framework/Framework.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework
+{
+    public class MyRandom
+    {
+        public int GetRandomNum1000()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            return r.Next(1000);
+        }
+
+        public UnityEngine.GameObject CreateOne()
+        {
+            UnityEngine.GameObject go = UnityEngine.GameObject.CreatePrimitive(UnityEngine.PrimitiveType.Cube);
+            go.transform.localPosition = UnityEngine.Vector3.zero;
+            return go;
+        }
+    }
+}
---
+++ Unity/Assets/TestMono.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Reflection;
+
+public class TestMono : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    Type type = Type.GetType("Mono.Runtime");
+		if (type != null)
+		{
+			MethodInfo info = type.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static);
+			
+			if (info != null)
+				Debug.Log(info.Invoke(null, null));
+		}
+
+        Rosetta.Rosetta r = new Rosetta.Rosetta();
+        Debug.LogWarning("Rosetta  100   " + r.GetRandomNum100());
+        Debug.LogWarning("Rosetta  1000   " + r.GetRandomNum1000());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private int count = 0;
+    public void OnClick()
+    {
+        Framework.MyRandom mr = new Framework.MyRandom();
+        Debug.LogWarning("Framework 1000   " + mr.GetRandomNum1000());
+
+        GameObject go = mr.CreateOne();
+        //go.transform.parent = this.transform;
+        go.name = "createone:" + count++;
+        go.transform.localPosition = Vector3.one * count;
+    }
+}
