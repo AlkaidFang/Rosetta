f73e2faf879001f245fcd402a17f4abcdf7a7c61
FangJun <alkaidfang@gmail.com>
2016-01-28 02:29:24 +0800

    deleted:    Framework/ExternalLibs/Protobuf-net/protobuf-net.dll 	modified:   Project.sln 	modified:   Rosetta/NetSystem/NetPacket.cs 	deleted:    Rosetta/NetSystem/PacketMessage/PacketMessage.cs 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/Licence.txt 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/common.xslt 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/csharp.xslt 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/descriptor.proto 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/protobuf-net.dll 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/protobuf-net.xml 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/protoc-license.txt 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/protogen.exe 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/protogen.exe.config 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/vb.xslt 	deleted:    Rosetta/NetSystem/PacketMessage/ProtoGen/xml.xslt 	deleted:    Rosetta/NetSystem/PacketMessage/XMessage.proto 	deleted:    Rosetta/NetSystem/PacketMessage/ZGenerateCode.cmd 	new file:   Rosetta/NetSystem/XMessage.cs 	modified:   Rosetta/Rosetta.csproj 	deleted:    Tools/protobuf-net/protobuf-net-r668.zip 	deleted:    Tools/protoc-2.6.1/address.pb.cc 	deleted:    Tools/protoc-2.6.1/address.pb.h 	deleted:    Tools/protoc-2.6.1/address.proto 	deleted:    Tools/protoc-2.6.1/protoc.exe 	deleted:    Tools/protoc-2.6.1/readme.txt 	deleted:    Tools/websocket-server/Fleck-0.11.1.zip

--- Rosetta/NetSystem/NetPacket.cs
+++ Rosetta/NetSystem/NetPacket.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Alkaid;
-using ProtoBuf;
 
 namespace Rosetta
 {
--- Rosetta/NetSystem/PacketMessage/ZGenerateCode.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-.\ProtoGen\protogen.exe -i:.\XMessage.proto -o:.\PacketMessage.cs
-:xcopy .\*.cs ..\ /y/d
-pause
